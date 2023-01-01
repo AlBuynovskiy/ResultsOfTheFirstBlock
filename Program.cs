b289e2e4d7e39f1618b58bf34c4abbb7eff73fa2
AlBuynovskiy <AlBuynovskiy@gmail.com>
2023-01-01 12:39:18 +0300

    Program debugging

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
 ﻿string[] m;
 Console.WriteLine("Введите массив строк через пробел: ");
-            string storeArray = Console.ReadLine();
-            m = storeArray.Split(' ');
-            var result = new string[m.Length];
-            var realSize = 0;
-            foreach (var value in m )
-            {
-                if (value.Length <= 3)
-                {
-                    result[realSize] = value;
-                    realSize++;
-                }
-            }
-            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
-            Console.ReadKey(true);
+string storeArray = Console.ReadLine();
+m = storeArray.Split(' ');
+var result = new string[m.Length];
+var realSize = 0;
+foreach (var value in m )
+{
+if (value.Length <= 3)
+    {
+    result[realSize] = value;
+    realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
